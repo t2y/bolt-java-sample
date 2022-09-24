024f09b0c53e40c37e844203506006b85eced0e9
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2022-09-24 14:37:55 +0900

    update to use quarkus as web application framework

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Bolt for Java sample
 
 * https://api.slack.com/start/building/bolt-java
+* https://slack.dev/java-slack-sdk/guides/supported-web-frameworks
 
 ## how to start
 
@@ -12,16 +13,24 @@ $ ngrok http 3000
 
 confirm the host name provided by ngrok.
 
-
 ```bash
-$ ./gradlew run -DslackLogLevel=debug
+$ ./gradlew quarkusDev 
 ...
-[main] INFO com.slack.api.bolt.jetty.SlackAppServer - ⚡️ Bolt app is running!
+__  ____  __  _____   ___  __ ____  ______
+ --/ __ \/ / / / _ | / _ \/ //_/ / / / __/
+ -/ /_/ / /_/ / __ |/ , _/ ,< / /_/ /\ \
+--\___\_\____/_/ |_/_/|_/_/|_|\____/___/
+2022-09-24 14:32:26,278 INFO  [io.quarkus] (Quarkus Main Thread) app unspecified on JVM (powered by Quarkus 2.12.0.Final) started in 1.111s. Listening on: http://localhost:3000
+2022-09-24 14:32:26,287 INFO  [io.quarkus] (Quarkus Main Thread) Profile dev activated. Live Coding activated.
+2022-09-24 14:32:26,287 INFO  [io.quarkus] (Quarkus Main Thread) Installed features: [cdi, config-yaml, servlet, smallrye-context-propagation, vertx]
 ```
 
-configure the URI to Event Subscriptions in slack app admin.
+configure the URI (`http://${YOUR_HOST}.ngrok.io/slack/events`) in slack app admin.
 
-open app home for bolt-java-sample in slack client.
+* Slash Commands
+* Event Subscriptions
+
+open app home for bolt-java-sample, or input `/hello` command on message box in your slack client.
 
 ## test
 
--- app/src/main/java/tutorial/bolt/sample/MyApp.java
+++ app/src/main/java/tutorial/bolt/sample/MyApp.java
@@ -1,47 +1,39 @@
 package tutorial.bolt.sample;
 
 import com.slack.api.bolt.App;
-import com.slack.api.bolt.jetty.SlackAppServer;
+import com.slack.api.bolt.response.Response;
+import com.slack.api.bolt.servlet.SlackAppServlet;
 import com.slack.api.model.event.AppHomeOpenedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static com.slack.api.model.block.Blocks.*;
-import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
-import static com.slack.api.model.block.composition.BlockCompositions.plainText;
-import static com.slack.api.model.block.element.BlockElements.asElements;
-import static com.slack.api.model.block.element.BlockElements.button;
-import static com.slack.api.model.view.Views.view;
+import javax.servlet.annotation.WebServlet;
 
-public class MyApp {
-    public static void main(String[] args) throws Exception {
-        var app = new App();
+@WebServlet("/slack/events")
+public class MyApp extends SlackAppServlet {
+    private static final Logger logger = LoggerFactory.getLogger(MyApp.class);
 
-        // All the room in the world for your code
+    private static final long serialVersionUID = 1L;
 
+    public MyApp() {
+        super(initSlackApp());
+    }
+
+    public MyApp(App app) {
+        super(app);
+    }
+
+    private static App initSlackApp() {
+        App app = new App();
+        app.command("/hello", (req, ctx) -> {
+            logger.info("/hello called!");
+            logger.debug("/hello debug");
+            return Response.ok(":wave: hello world");
+        });
         app.event(AppHomeOpenedEvent.class, (payload, ctx) -> {
-            var appHomeView = view(view -> view
-                    .type("home")
-                    .blocks(asBlocks(
-                            section(section -> section.text(markdownText(mt -> mt.text("*Welcome to your _App's Home_* :tada:")))),
-                            divider(),
-                            section(section -> section.text(markdownText(mt -> mt.text("This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example on <https://slack.dev/java-slack-sdk/guides/interactive-components|slack.dev/java-slack-sdk>.")))),
-                            actions(actions -> actions
-                                    .elements(asElements(
-                                            button(b -> b.text(plainText(pt -> pt.text("Click me!"))).value("button1").actionId("button_1"))
-                                    ))
-                            )
-                    ))
-            );
-
-            var res = ctx.client().viewsPublish(r -> r
-                    .userId(payload.getEvent().getUser())
-                    .view(appHomeView)
-            );
-
+            logger.debug("app home opened!");
             return ctx.ack();
         });
-
-
-        var server = new SlackAppServer(app);
-        server.start();
+        return app;
     }
 }
